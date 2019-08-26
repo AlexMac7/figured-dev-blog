5cc25e071af7ebeee5ac257b28bb15769c906ec5
Alex MacKenzie <alexbcmac7@gmail.com>
2019-08-26 16:45:01 -0700

    Update users model and factory

--- app/User.php
+++ app/User.php
@@ -4,26 +4,27 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Jenssegers\Mongodb\Eloquent\HybridRelations;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HybridRelations;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    protected $connection = 'mysql';
+
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
