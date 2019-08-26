ee54b1e911329b22456c864ac5075484d9c0b35d
Alex MacKenzie <alexbcmac7@gmail.com>
2019-08-26 16:48:03 -0700

    Create post controller and test

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+
+class PostController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth')->except('index');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::with('user')->paginate(5);
+
+        return response()->json(['posts' => $posts]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => ['required', 'string', 'min:2'],
+            'description' => ['required', 'string', 'min:2']
+        ]);
+
+        Post::create([
+            'title' => $validatedData['title'],
+            'description' => $validatedData['description'],
+            'user_id' => auth()->user()->id
+        ]);
+
+        return response()->json([], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  str  $postId
+     * @return \Illuminate\Http\Response
+     */
+    public function show($postId)
+    {
+        $post = Post::findOrFail($postId);
+
+        abort_unless(Gate::allows('update-post', $post), 403);
+
+        return response()->json(['post' => $post], 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  str  $postId
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $postId)
+    {
+        $post = Post::findOrFail($postId);
+
+        abort_unless(Gate::allows('update-post', $post), 403);
+
+        $validatedData = $request->validate([
+            // TODO: Any max length with mongodb? Also text may not be a valid type in Mongo, looks like string is used
+            'title' => ['required', 'string', 'min:2'],
+            'description' => ['required', 'string', 'min:2']
+        ]);
+
+        $post->update([
+            'title' => $validatedData['title'],
+            'description' => $validatedData['description']
+        ]);
+
+        return response()->json([], 204);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  str  $postId
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($postId)
+    {
+        $post = Post::findOrFail($postId);
+
+        abort_unless(Gate::allows('delete-post', $post), 403);
+
+        $post->delete();
+
+        return response()->json([], 200);
+    }
+}
---
+++ tests/Feature/PostControllerTest.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Post;
+use App\User;
+use Tests\RefreshMongoDatabase;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PostControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->artisan('migrate:fresh', [ // TODO: Override RefreshDatabase trait and combine
+            '--path' => 'database/migrations/mongodb',
+            '--database' => 'mongodb',
+        ]);
+
+        $this->user = factory(User::class)->create();
+
+        factory(Post::class, 15)->create([
+            'user_id' => $this->user->id
+        ]);
+
+        $this->post = Post::first();
+    }
+
+    /** @test */
+    public function anyone_can_get_all_posts()
+    {
+        $response = $this->json('GET', '/posts');
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'posts' => [
+                    'current_page',
+                    'first_page_url',
+                    'from',
+                    'last_page',
+                    'last_page_url',
+                    'next_page_url',
+                    'path',
+                    'per_page',
+                    'prev_page_url',
+                    'to',
+                    'total',
+                    'data' => [
+                        [
+                            '_id',
+                            'user_id',
+                            'description',
+                            'updated_at',
+                            'created_at'
+                        ]
+                    ]
+                ]
+            ])
+            ->assertJsonFragment([
+                '_id' => $this->post->_id,
+                'user_id' => $this->post->user_id,
+                'description' => $this->post->description,
+            ]);
+    }
+
+    // TODO: Test show method
+
+    /** @test */
+    public function authenticated_user_can_create_post()
+    {
+        $data = [
+            'title' => 'New Post',
+            'description' => 'Create a new post.'
+        ];
+
+        $response = $this->actingAs($this->user)->json('POST', '/posts', $data);
+
+        $response->assertStatus(201);
+
+        $this->assertDatabaseHas('posts', [
+            'user_id' => $this->user->id,
+            'title' => $data['title'],
+            'description' => $data['description'],
+        ], 'mongodb');
+    }
+
+    /** @test */
+    public function unauthenticated_user_can_not_create_post()
+    {
+        $data = [
+            'title' => 'New Title',
+            'description' => 'Create a new post.'
+        ];
+
+        $response = $this->json('POST', '/posts', $data);
+
+        $response->assertStatus(401);
+    }
+
+    /** @test */
+    public function authenticated_user_can_update_post()
+    {
+        $data = [
+            'title' => 'Update Title',
+            'description' => 'Update an existing post.'
+        ];
+
+        $response = $this->actingAs($this->user)->json('PATCH', sprintf('/posts/%s', $this->post->id), $data);
+
+        $response->assertStatus(204);
+
+        $this->assertDatabaseHas('posts', [
+            'user_id' => $this->user->id,
+            'title' => $data['title'],
+            'description' => $data['description'],
+        ], 'mongodb');
+    }
+
+    /** @test */
+    public function unauthenticated_user_can_not_update_post()
+    {
+        $data = [
+            'title' => 'Update Title',
+            'description' => 'Update an existing post.'
+        ];
+
+        $response = $this->json('PATCH', sprintf('/posts/%s', $this->post->id), $data);
+
+        $response->assertStatus(401);
+    }
+
+    /** @test */
+    public function authenticated_can_not_update_another_users_post()
+    {
+        $post = factory(Post::class)->create();
+
+        $data = [
+            'title' => 'Update Title',
+            'description' => 'Update an existing post.'
+        ];
+
+        $response = $this->actingAs($this->user)->json('PATCH', sprintf('/posts/%s', $post->id), $data);
+
+        $response->assertStatus(403);
+    }
+
+    /** @test */
+    public function authenticated_user_can_delete_post()
+    {
+        $response = $this->actingAs($this->user)->json('DELETE', sprintf('/posts/%s', $this->post->id));
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseMissing('posts', [
+            'user_id' => $this->user->id,
+            'title' => $this->post->title,
+            'description' => $this->post->description
+        ], 'mongodb');
+    }
+
+    /** @test */
+    public function unauthenticated_user_can_not_delete_post()
+    {
+        $response = $this->json('DELETE', sprintf('/posts/%s', $this->post->id));
+
+        $response->assertStatus(401);
+    }
+
+    /** @test */
+    public function authenticated_user_can_not_delete_another_users_post()
+    {
+        $post = factory(Post::class)->create();
+
+        $response = $this->actingAs($this->user)->json('DELETE', sprintf('/posts/%s', $post->id));
+
+        $response->assertStatus(403);
+    }
+}
