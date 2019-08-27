acbd05495d7751327f7808fdd77c2117ede4be5b
Alex MacKenzie <alexbcmac7@gmail.com>
2019-08-26 21:58:42 -0700

    Order posts by latest from index method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -25,7 +25,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::with('user')->paginate(5);
+        $posts = Post::with('user')->latest()->paginate(5);
 
         return response()->json(['posts' => $posts]);
     }
